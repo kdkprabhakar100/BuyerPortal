8a53391aacdc5657ffcbab54da5d82e2530edefa
kdkprabhakar100 <kdkprabhakar100@gmail.com>
2026-03-27 23:25:52 +0545

    mongoDB atlas connection

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Server + DB working ");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
